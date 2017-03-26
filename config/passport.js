4aa2e3e4c7036f759bb8a88cc94830dd54d273d7
jamorocho3 <jersonmorocho16@gmail.com>
2017-03-26 09:18:40 -0500

    Correcion de Errores en el codigo

--- config/passport.js
+++ config/passport.js
@@ -4,19 +4,18 @@ const User = require('../modelos/modelo.Usuario.js');
 const config = require('../config/db');
 var jwt = require('jsonwebtoken');
 
-module.exports=function(passport) {
-    var opts={};
+module.exports = (passport) => {
+    var opts = {};
     opts.jwtFromRequest = ExtractJwt.fromAuthHeader();
     opts.secretOrKey = "capob";
-    passport.use(new JwtStrategy(opts,(jwt_payload,done)=>{
-        console.log("payload receibed:",jwt_payload);
-        User.getUserById(jwt_payload._doc._id,(err,user)=>{
+    passport.use(new JwtStrategy(opts,(jwt_payload,done) => {
+        console.log("Payload Recibido:",jwt_payload);
+        User.getUserById(jwt_payload._doc._id,(err,user) => {
             if(err) {
                 return done(err,false);
-            }            
+            }
             if(user) {
-
-                console.log("USuario listo");
+                console.log("Usuario Listo");
                 return done(null,user);
             } else {
                 return done(null,false);
